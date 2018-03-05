9b257995f4216c7028aa670565ca0acf1ee69d89
Rafael Zarate <rafazrte@gmail.com>
2018-03-05 10:12:23 -0800

    Final revision

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: rzarate <rzarate@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/05 01:14:57 by rzarate           #+#    #+#             */
-/*   Updated: 2018/03/05 09:54:49 by rzarate          ###   ########.fr       */
+/*   Updated: 2018/03/05 10:03:05 by rzarate          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,44 +14,50 @@
 
 int	get_next_line(const int fd, char **line)
 {
-	static	char	*c_buff = NULL;
+	static	char	*c_buff[5000];
 	int				ret;
 	char			buff[BUFF_SIZE];
+	char			*tmp;
 	int				i;
 
 	ret = 0;
 	if (fd < 0 || !line)
 		return (-1);
-	if (c_buff == NULL)
-		c_buff = ft_strnew(0);
-	if (!ft_strchr(c_buff, '\n'))
+	if (c_buff[fd] == NULL)
+		c_buff[fd] = ft_strnew(0);
+	if (!ft_strchr(c_buff[fd], '\n'))
 	{
 		while ((ret = read(fd, buff, BUFF_SIZE)) > 0)
 		{
 			buff[ret] = '\0';
-			c_buff = ft_strjoin(c_buff, buff);
-			if (ft_strchr(c_buff, '\n'))
+			tmp = ft_strjoin(c_buff[fd], buff);
+			free(c_buff[fd]);
+			c_buff[fd] = tmp;
+			if (ft_strchr(c_buff[fd], '\n'))
 				break ;
 		}
 	}
 	if (ret < 0)
 		return (-1);
-	else if (ret == 0 && (!c_buff[0] || !c_buff))
+	else if (ret == 0 && (!c_buff[fd][0] || !c_buff[fd]))
 		return (0);
 	else
 	{
 		i = 0;
-		while (c_buff[i] && c_buff[i] != '\n')
+		while (c_buff[fd][i] && c_buff[fd][i] != '\n')
 			i++;
-		if (c_buff[i] == '\n')
+		if (c_buff[fd][i] == '\n')
 		{
-			*line = ft_strsub(c_buff, 0, i);
-			c_buff = ft_strsub(c_buff, i + 1, ft_strlen(c_buff) - i - 1);
+			*line = ft_strsub(c_buff[fd], 0, i);
+			tmp = ft_strsub(c_buff[fd], i + 1, ft_strlen(c_buff[fd]) - i - 1);
+			free(c_buff[fd]);
+			c_buff[fd] = tmp;
 		}
-		else if (c_buff[i] == '\0')
+		else if (c_buff[fd][i] == '\0')
 		{
-			*line = ft_strsub(c_buff, 0, i);
-			c_buff = NULL;
+			*line = ft_strsub(c_buff[fd], 0, i);
+			free(c_buff[fd]);
+			c_buff[fd] = NULL;
 		}
 		return (1);
 	}
--- get_next_line.h
+++ get_next_line.h
@@ -1,11 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rzarate <rzarate@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/03/05 10:05:34 by rzarate           #+#    #+#             */
+/*   Updated: 2018/03/05 10:06:52 by rzarate          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#ifndef GNL_H
-# define GNL_H
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
 # include "libft/libft.h"
 # include "fcntl.h"
 # define BUFF_SIZE 2000
-
 
 int	get_next_line(const int fd, char **line);
 
